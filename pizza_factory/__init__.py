dd772a6946cc67daca6787aa1ed733b71df33c41
BorjaEst <boressan@outlook.com>
2021-11-24 20:00:32 +0100

    Pass tests

--- pizza_factory/__init__.py
+++ pizza_factory/__init__.py
@@ -2,8 +2,16 @@ __all__ = ["Factory", "Pizza"]
 
 
 class Factory:
-    pass
+    def __init__(self, location, recipes) -> None:
+        self.location = location
+        self.recipes = recipes
+
+    def make(self, order, name):
+        toppings = self.recipes["pizza"][name]
+        return order(name, toppings)
 
 
 class Pizza:
-    pass
+    def __init__(self, name, toppings) -> None:
+        self.name = name
+        self.toppings = toppings
